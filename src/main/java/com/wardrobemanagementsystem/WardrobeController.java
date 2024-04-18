5517b8f5d5540be8cbe12dbcfdc844f99b7f746b
MarcRGM <marcrodniemanalo@gmail.com>
2024-04-18 09:31:50 +0800

    Experiment with FileChooser

--- src/main/java/com/wardrobemanagementsystem/WardrobeController.java
+++ src/main/java/com/wardrobemanagementsystem/WardrobeController.java
@@ -1,12 +1,29 @@
 package com.wardrobemanagementsystem;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class WardrobeController {
 
     @FXML
     private Pane paneShirtInfo, paneDressInfo;
+
+
+
+    // Experiment
+    @FXML
+    private ImageView imageChange;
+
+    @FXML
+    private Button btnImage;
+
+
 
     @FXML
     protected void btnShirt() {
@@ -24,6 +41,24 @@ public class WardrobeController {
     protected void btnCloseInfo() {
         paneShirtInfo.setVisible(false);
         paneDressInfo.setVisible(false);
+    }
+
+
+    // Experiment
+    @FXML
+    protected void btnAddImage() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose Image File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif")
+        );
+
+        File selectedFile = fileChooser.showOpenDialog(btnImage.getScene().getWindow());
+
+        if (selectedFile != null) {
+            Image image = new Image(selectedFile.toURI().toString());
+            imageChange.setImage(image);
+        }
     }
 
 }
