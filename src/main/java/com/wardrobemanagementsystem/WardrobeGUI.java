f7fa30ecc7d42567257a72187c8e2ad9d3b3d247
MarcRGM <marcrodniemanalo@gmail.com>
2025-05-24 10:54:03 +0800

    Add icon

--- src/main/java/com/wardrobemanagementsystem/WardrobeGUI.java
+++ src/main/java/com/wardrobemanagementsystem/WardrobeGUI.java
@@ -5,20 +5,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class WardrobeGUI extends Application {
 
+    // ADD FEATURE WHERE IF YOU DELETE AN ITEM IT WILL ALSO DELETE THE ITEM ON THE SHOW OUTFIT SECTION
+
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Wardrobe Management System");
         stage.setResizable(false);
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/wardrobemanagementsystem/logo.png")));
 
          // Load the splash screen FXML
         Parent splash = FXMLLoader.load(WardrobeGUI.class.getResource("splash.fxml"));
@@ -27,26 +26,27 @@ public class WardrobeGUI extends Application {
 
         stage.setScene(splashScene);
         stage.setResizable(false);
-        stage.centerOnScreen();
-        stage.initStyle(StageStyle.DECORATED);
+
         stage.show();
 
-
-        // Load the wardrobe FXML
+        // Load and display the wardrobe FXML
         PauseTransition delay = new PauseTransition(Duration.seconds(6)); //
         delay.setOnFinished(event -> {
             try {
                 Parent wardrobeGUI = FXMLLoader.load(WardrobeGUI.class.getResource("wardrobe.fxml"));
 
                 Scene mainScene = new Scene(wardrobeGUI);
+
                 stage.setScene(mainScene);
                 stage.setResizable(false);
-                stage.centerOnScreen();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
         delay.play();
+
+
+
 
     }
 
